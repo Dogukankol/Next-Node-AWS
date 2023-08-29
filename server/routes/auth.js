a2734fb4698d03da5b5379f107af956c6cdc5d72
Doğukan Kol <43730866+Dogukankol@users.noreply.github.com>
2023-08-30 01:31:02 +0300

    verification and login added

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,13 @@
 import express from "express";
 import {
   createUser,
+  verification,
+  login
   } from "../controllers/auth.js";
   const router = express.Router();
   
-  router.post("/register", createUser);
+  router.post("/account/register", createUser);
+  router.post("/account/verification", verification);
+  router.post("/account/login", login);
   
   export default router;
